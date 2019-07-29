114cd2bf9750a12f842e9c23a330d34d1250eeff
Ava DeLaCruz <avad333@gmail.com>
2019-07-28 21:39:03 -0700

    added nav bar color change w/ jquery -> will change to use classes later

--- basic.js
+++ basic.js
@@ -4,13 +4,30 @@ var links;
 $(function(){
   var navbar = document.getElementById("site-nav-bar");
   links = navbar.getElementsByTagName("li");
+  // alert(window.location.pathname);
+  var pattern = new RegExp("[^/]+$");
+  var page = pattern.exec(window.location.pathname);
+  $('ul li a').each(function() {
+    if($(this).attr("href")===page[0]){
+      $(this).css("color", "#D77771");
+    }
+    else{
+      $(this).css("color", "black");
+    }
+  });
+
 });
 
 
-$("#site-nav-bar").click(function(){
-  for(var i=0; i<links.length; i++){
 
 
-  }
-
-});
+// $("#site-nav-bar").click(function(){
+//
+//   // for(var i=0; i<links.length; i++){
+//     // $(this).html("hi");
+//
+//
+//
+//   }
+//
+// });
