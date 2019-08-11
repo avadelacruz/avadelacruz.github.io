2bb35bbe98edc7503a9a04303a1414b1ce7dbd7f
Ava DeLaCruz <avad333@gmail.com>
2019-08-11 13:12:22 -0700

    start persistent theme

--- basic.js
+++ basic.js
@@ -1,46 +1,28 @@
-var links;
-var lightModeOn;
-var currentPageName;
-
-// on page load
 $(function(){
-  //set active link color for current page
-  var navbar = document.getElementById("site-nav-bar");
-  links = navbar.getElementsByTagName("li");
-  // alert(window.location.pathname);
-  var pattern = new RegExp("[^/]+$");
-  var page = pattern.exec(window.location.pathname);
-  currentPageName = page[0];
-  $('ul li a').each(function() {
-    if($(this).attr("href")===currentPageName){
-      $(this).css("color", "#D77771");
-    }
-    else{
-      // $(this).css("color", "black");
-    }
-  });
+  document.getElementsByTagName("html")[0].style.visibility = "visible";
 });
-
 
 $("#slider").change(function(){
   //slider will default to checked, but on first change will be unchecked
   //box goes from checked->unchecked as user goes from light->dark mode
-  lightModeOn = this.checked;
+  var lightModeOn = this.checked;
   if(lightModeOn){
     //turn on dark mode
     document.getElementById("light-css").disabled = true;
     document.getElementById("dark-css").disabled = false;
     //if on dark mode and on resume page, load dark resume
-    if(currentPageName=="resume.html"){
+    if(localStorage["current-page-name"]=="resume.html"){
       $("#resume-picture").attr("src", "DeLaCruz_Ava_Dark.jpeg");
     }
+    localStorage["current-stylesheet"] = "dark-css";
   }
   else{
     //turn on light mode
     document.getElementById("light-css").disabled = false;
     document.getElementById("dark-css").disabled = true;
-    if(currentPageName=="resume.html"){
+    if(localStorage["current-page-name"]=="resume.html"){
       $("#resume-picture").attr("src", "DeLaCruz_Ava.jpeg");
-    }  
+    }
+    localStorage["current-stylesheet"] = "light-css";
   }
-})
+});
---
+++ onload.js
@@ -0,0 +1,33 @@
+//on doc load
+$(function(){
+
+  //set active link color for current page
+  var navbar = document.getElementById("site-nav-bar");
+  var links = navbar.getElementsByTagName("li");
+  var pattern = new RegExp("[^/]+$");
+  var page = pattern.exec(window.location.pathname);
+  localStorage["current-page-name"] = page[0];
+  $('ul li a').each(function() {
+    if($(this).attr("href")===localStorage["current-page-name"]){
+      $(this).css("color", "#D77771");
+    }
+  });
+  //if user chose dark theme previously, apply dark theme
+  if(localStorage["current-stylesheet"]==="dark-css"){
+    document.getElementById("light-css").disabled = true;
+    document.getElementById("dark-css").disabled = false;
+    $("#slider").prop("checked", true);
+    if(localStorage["current-page-name"]=="resume.html"){
+      $("#resume-picture").attr("src", "DeLaCruz_Ava_Dark.jpeg");
+    }
+  }
+  else{ //if user chose light theme previously, apply light theme
+    document.getElementById("light-css").disabled = false;
+    document.getElementById("dark-css").disabled = true;
+    $("#slider").prop("checked", false);
+    if(localStorage["current-page-name"]=="resume.html"){
+      $("#resume-picture").attr("src", "DeLaCruz_Ava.jpeg");
+    }
+  }
+
+});
