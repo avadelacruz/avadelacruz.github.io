36a788996643af3717f807d860ec2a232ac2e16a
Ava DeLaCruz <avad333@gmail.com>
2020-02-01 13:20:12 +0100

    fixed link highlighting

--- scripts/onload.js
+++ scripts/onload.js
@@ -1,19 +1,29 @@
 function setActiveLinkColor() {
+	console.log("hi");
 	//set active link color for current page
-	var pattern = new RegExp("[^/]+$");
-	var page = pattern.exec(window.location.pathname);
+	var pagePattern = new RegExp("([^/]+)(?=[^/]*/?$)");
+	var page = pagePattern.exec(window.location.pathname);
 	if (page == null) {
-		localStorage["current-page-name"] = "index.html";
+		localStorage["current-page-name"] = "";
 	} else {
 		localStorage["current-page-name"] = page[0];
 	}
+
 	$("ul li a").each(function() {
-		if ($(this).attr("href") === localStorage["current-page-name"]) {
-			if (localStorage["current-stylesheet"] === "dark-css") {
-				$(this).css("color", "#f0a39e");
-			} else {
-				$(this).css("color", "#D77771");
-			}
+		console.log($(this).attr("href"));
+		let linkPattern = new RegExp("[^/.]+");
+		let linkName = linkPattern.exec($(this).attr("href"));
+		console.log(linkName);
+		if (
+			linkName !== null &&
+			linkName[0] === localStorage["current-page-name"]
+		) {
+			// console.log("TRUE!!!");
+			// if (localStorage["current-stylesheet"] === "dark-css") {
+			$(this).addClass("active");
+			// } else {
+			// 	$(this).css("color", "#D77771");
+			// }
 		}
 	});
 }
@@ -27,18 +37,18 @@ $(function() {
 	if (localStorage["current-stylesheet"] === "dark-css") {
 		// $("#theme").attr("href", "../styles/dark.css");
 
-		localStorage["current-page-name"] == "index.html"
+		localStorage["current-page-name"] == ""
 			? $("#theme").attr("href", "./styles/dark.css")
 			: $("#theme").attr("href", "../styles/dark.css");
 
 		$("#slider").prop("checked", true);
-		if (localStorage["current-page-name"] == "resume.html") {
+		if (localStorage["current-page-name"] == "resume") {
 			$("#resume-picture").attr("src", "../images/DeLaCruz_Ava_Dark.jpeg");
 		}
 	} else {
 		//if user chose light theme previously, apply light theme
 		$("#slider").prop("checked", false);
-		if (localStorage["current-page-name"] == "resume.html") {
+		if (localStorage["current-page-name"] == "resume") {
 			$("#resume-picture").attr("src", "../images/DeLaCruz_Ava.jpeg");
 		}
 	}
